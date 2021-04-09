c637ee7041af29e8fcee3a53e6d06a3c3cf7c1b2
Bhargavi Saikia <bhargavisaikia187@gmail.com>
2021-04-09 21:28:08 +0530

    add lines to build the interface

--- length_Converter.py
+++ length_Converter.py
@@ -6,4 +6,8 @@ window.configure(background="light blue")
 window.geometry("320x220")
 window.resizable(width=False, height=False)
 
+feet_label=Label(window, text="Feet", bg="purple", fg="white", width=15)
+feet_label.grid(column=0, row=0, padx=15, pady=15)
+
+
 window.mainloop()

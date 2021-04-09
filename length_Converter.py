a3185acde8a219f8df4bf9dbf57d75c3fcfd7863
Bhargavi Saikia <bhargavisaikia187@gmail.com>
2021-04-09 21:36:49 +0530

    build interface of the app, design value box for feet

--- length_Converter.py
+++ length_Converter.py
@@ -9,5 +9,8 @@ window.resizable(width=False, height=False)
 feet_label=Label(window, text="Feet", bg="purple", fg="white", width=15)
 feet_label.grid(column=0, row=0, padx=15, pady=15)
 
-
+feet_value=DoubleVar()
+feet_entry=Entry(window, textvariable="feet_value", width=15)
+feet_entry.grid(column=1, row=0)
+feet_entry.delete(0, "end")
 window.mainloop()

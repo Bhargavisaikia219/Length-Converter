af467b821c886e4e29c52a866174db9abd5f75ac
Bhargavi Saikia <bhargavisaikia187@gmail.com>
2021-04-09 21:13:55 +0530

    building the background of the app

---
+++ length_Converter.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+window=Tk()
+window.title("Feet to Meter Conversion App")
+window.configure(background="light blue")
+window.geometry("320x220")
+window.resizable(width=False, height=False)
+
+window.mainloop()

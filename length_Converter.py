a0b713d4134b92aeb70a32b91e23d6194f2cc4a7
Bhargavi Saikia <bhargavisaikia187@gmail.com>
2021-04-10 23:01:36 +0530

    add convert and clear button

--- length_Converter.py
+++ length_Converter.py
@@ -14,18 +14,22 @@ unit_label.grid(column=0, row=0, padx=15, pady=15)
 
 source_value=DoubleVar()
 source_value_entry=Entry(window, textvariable="source_value", width=15)
-source_value_entry.grid(column=1, row=0)
+source_value_entry.grid(column=1, row=0, pady=25)
 
 result_unit=StringVar()
 result_label=ttk.Combobox(window, textvariable=result_unit, width=15)
 result_label["value"]=("milimeter", "centimeter", "meter", "kilometer", "inch", "feet", "yard", "mile")
-result_label.grid(column=0, row=1, padx=15, pady=15)
+result_label.grid(column=0, row=1, padx=15, pady=30)
 
 result_value=DoubleVar()
 result_entry=Entry(window, textvariable="result_value", width=15)
-result_entry.grid(column=1, row=1)
+result_entry.grid(column=1, row=1, pady=30)
 result_entry.delete(0, "end")
 
+convert=Button(window, text="Convert", bg="blue", fg="white", width=15, command="convert")
+convert.grid(column=0, row=3, padx=15, pady=3)
 
+clear=Button(window, text="Clear", bg="grey", fg="white", width=15, command="")
+clear.grid(column=1, row=3, padx=15, pady=3)
 
 window.mainloop()

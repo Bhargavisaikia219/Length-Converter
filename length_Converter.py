341d3c2a79e051cfa72f7aa682d8895a6932c62f
Bhargavi Saikia <bhargavisaikia187@gmail.com>
2021-04-13 01:19:58 +0530

    fix bugs, add dictionary and take key and value as inputs, implementand test set function to get the output

--- length_Converter.py
+++ length_Converter.py
@@ -2,10 +2,18 @@ from tkinter import *
 from tkinter import ttk
 
 def convertunit():
+    SI = {'millimeter': 0.001, 'centimeter': 0.01, 'meter': 1, 'kilometer': 1000,
+          'inch': 0.0254, 'feet': 0.3048, 'yard': 0.9144, 'mile': 1609.35}
 
-    value_input = unit_label.get()
-    value_output = result_label.get()
-    print(value_input, value_output)
+    unit_input = unit_label.get()
+    unit_output = result_label.get()
+    value_input = float(source_value_entry.get())
+
+    if unit_input in SI.keys() and unit_output in SI.keys():
+        input_ratio = float(SI.get(unit_input))
+        output_ratio = float(SI.get(unit_output))
+        result = value_input * input_ratio / output_ratio
+        result_value.set("%.4f" % result)
 
 window = Tk()
 window.title("Length Conversion App")
@@ -16,10 +24,10 @@ window.resizable(width=False, height=False)
 length_unit = StringVar()
 unit_label = ttk.Combobox(window, textvariable=length_unit, width=15 )
 unit_label["value"] = ("millimeter", "centimeter", "meter", "kilometer", "inch", "feet", "yard", "mile")
-unit_label.grid(column=0, row=0, padx=15, pady=15)
+unit_label.grid(column=0, row=0, padx=35, pady=35)
 
 source_value = DoubleVar()
-source_value_entry = Entry(window, textvariable="source_value", width=15)
+source_value_entry = Entry(window, textvariable=source_value, width=15)
 source_value_entry.grid(column=1, row=0, pady=25)
 
 result_unit = StringVar()
@@ -28,7 +36,7 @@ result_label["value"] = ("millimeter", "centimeter", "meter", "kilometer", "inch
 result_label.grid(column=0, row=1, padx=15, pady=30)
 
 result_value = DoubleVar()
-result_entry = Entry(window, textvariable="result_value", width=15)
+result_entry = Entry(window, textvariable=result_value, width=15)
 result_entry.grid(column=1, row=1, pady=30)
 result_entry.delete(0, "end")
 
